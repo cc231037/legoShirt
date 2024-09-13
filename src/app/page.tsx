907d866d9f08e18e9563bbd08f9bcece1fe14c9b
Karl Schodder <cc231037@fhstp.ac.at>
2024-09-13 10:32:07 +0200

    finishing TOUCH

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
               </div>
               <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold 
               !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
-                Your Image on a <span className="bg-red-600 px-2 text-white">Custom</span> LEGO Minifigure
+                Your Image on a Custom <span className="bg-red-600 px-2 text-white">LEGO</span> Minifigure
               </h1>
               <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
                 Design your own LEGO parts with your <span className="font-semibold">one-of-one</span> Minifigure Torso. 
